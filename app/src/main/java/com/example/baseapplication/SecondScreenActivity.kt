194e16bcec86283f880300593fa9266a9328e8dc
yuhei.k <yuhei.k@gmail.com>
2020-04-23 16:52:11 +0900

    Add SecondScreen.

---
+++ app/src/main/java/com/example/baseapplication/SecondScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.baseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_screen)
+    }
+}
